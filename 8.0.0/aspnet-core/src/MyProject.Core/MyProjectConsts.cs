c7724472f5310efa58a3895b43015f24e3b78383
duongtd <duong.tranduc@ncc.asia>
2023-03-13 22:01:10 +0700

    first commit

---
+++ 8.0.0/aspnet-core/src/MyProject.Core/MyProjectConsts.cs
@@ -0,0 +1,20 @@
+﻿using MyProject.Debugging;
+
+namespace MyProject
+{
+    public class MyProjectConsts
+    {
+        public const string LocalizationSourceName = "MyProject";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+
+
+        /// <summary>
+        /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+        /// </summary>
+        public static readonly string DefaultPassPhrase =
+            DebugHelper.IsDebug ? "gsKxGZ012HLL3MI5" : "1a8ac1aceb1646c5a95a4ecfe0e71c59";
+    }
+}
